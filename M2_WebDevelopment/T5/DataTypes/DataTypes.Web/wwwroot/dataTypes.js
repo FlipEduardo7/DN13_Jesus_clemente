bae052162c9a53af6970b83143d9fa25ac96f901
FlipEduardo7 <langos_cr7@hotmail.com>
2024-04-22 12:41:44 -0600

    upload T5 from M2

---
+++ M2_WebDevelopment/T5/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,37 @@
+﻿function onButtonClick(evt) {
+    let b = evt.currentTarget;
+
+    let classes = b.classList;
+
+    let isGreen = false;
+
+    for (let i = 0; i < classes.length; i++) {
+
+        let c = classes[i];
+
+        if (c == "greenButton") {
+            isGreen = true;
+            break;  
+        }
+    }
+
+    if (isGreen) {
+        b.className = "btn redButton";
+    }
+    else {
+        b.className = "btn greenButton";
+    }
+
+    let t = document.getElementById("sampleTable");
+    let currentHtml = t.innerHTML;
+
+    t.innerHTML += "<tr><td>" + b.getAttribute("name") + "</td><td>" + new Date() + "</td><tr>";
+}
+
+let buttons = document.getElementsByTagName("button");
+
+for (let i = 0; buttons.length; i++) {
+    buttons[i].addEventListener("click", onButtonClick);
+}
+
+//button.addEventListener("click", onButtonClick);
---
+++ M2_WebDevelopment/T5/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "This is a String ";
+            int age = 35;
+            DateTime now = DateTime.Now;
+
+            double amount = 0;
+
+            //variables y formato tipo string
+            string string2 = text + " a second string " + age + now + amount;
+
+            string string3 = string.Format("The age is {0}, the time is {1}, and the amount is {2}", age, now, amount);
+
+            string string4 = $"The age is {age}, the time is {now}, and the amount is {amount}";
+
+            //varibles tipo char
+            char sampleChar = 'c';
+            char[] arrayChar = string4.ToCharArray();
+
+            amount = (double)10 / (double)3;
+
+            //fecha
+            DateTime dateTime = new DateTime(2000, 1, 1);
+
+            //intervalo de tiempo entre fechas
+            TimeSpan timeStamp1 = now - dateTime;
+
+            Console.WriteLine(timeStamp1.Days);
+
+            string test = "15";
+            age = int.Parse(test);
+
+            string booleanValue = "true";
+            bool isTrue = bool.Parse(booleanValue);
+
+            Console.ReadKey();
+        }
+    }
+}
