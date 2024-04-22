3eabb55681f3122dd0f4bec101f4b41add7c5c52
FlipEduardo7 <langos_cr7@hotmail.com>
2024-04-22 10:57:09 -0600

    upload T4 from M2

---
+++ M2_WebDevelopment/T4/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,25 @@
+﻿let element = document.getElementById("example");
+let textExample = "this is a text";
+let aNumber = 10.333;
+
+let complexObject = {
+    name: "Israel",
+    lastName: "Perez",
+    age: 35
+}
+
+let text1 = "text";
+let text2 = "text";
+//let ul = "<ul>";
+
+//for (let i = 0; i < textExample.length; i++) {
+//    ul += ("<li>" + textExample[i] + "</li>");
+//}
+
+//ul += "</ul>";
+
+let data = "133333";
+
+
+
+element.innerHTML = typeof (parseInt(data));
