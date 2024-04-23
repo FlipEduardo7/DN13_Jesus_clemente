8316c8f21d8a61fca87d215f8309952b38dbf6e3
FlipEduardo7 <langos_cr7@hotmail.com>
2024-04-23 16:50:17 -0600

    upload E2 from M2

---
+++ M2_WebDevelopment/E2/GymManager Web/GymManager Web/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetService<IConfiguration>();
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute("MemberIn", "/Attendance/MemberIn", new { controller = "Attendance", action = "MemberIn" });
+    endpoints.MapControllerRoute("MemberOut", "/Attendance/MemberOut", new { controller = "Attendance", action = "MemberOut" });
+});
+
+app.Run();
