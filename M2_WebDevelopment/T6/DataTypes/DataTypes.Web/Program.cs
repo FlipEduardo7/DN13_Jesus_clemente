2b8b4c699fa5e54fadf8f42935046797173352ad
FlipEduardo7 <langos_cr7@hotmail.com>
2024-04-22 18:42:08 -0600

    upload T6 from M2

---
+++ M2_WebDevelopment/T6/DataTypes/DataTypes.Web/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetService<IConfiguration>();
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
