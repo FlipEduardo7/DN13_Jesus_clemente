f9ef3ed6b490b56131a4eada6a8e69201bc15a9f
FlipEduardo7 <langos_cr7@hotmail.com>
2024-05-29 08:34:06 -0600

    upload T7 from M3

---
+++ M3_NetProjectsWebApi/T7/GymManager Web/GymManager Web/Program.cs
@@ -0,0 +1,60 @@
+using GymManager.ApplicationServices.Members;
+using GymManager.ApplicationServices.Navigation;
+using GymManager.DataAccess;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetService<IConfiguration>();
+
+string connectionString = configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<GymManagerContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<GymManagerContext>();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.LoginPath = "/Account/Login";
+    options.SlidingExpiration = true;
+});
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//    .AddCookie(options =>
+//    {
+//        options.LoginPath = "/Account/Login";
+//        options.AccessDeniedPath = "/";
+//    });
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddTransient<IMembersAppService, MembersAppService>();
+builder.Services.AddTransient<IMenuAppService, MenuAppService>();
+
+
+
+var app = builder.Build();
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute("MemberIn", "/Attendance/MemberIn", new { controller = "Attendance", action = "MemberIn" });
+    endpoints.MapControllerRoute("MemberOut", "/Attendance/MemberOut", new { controller = "Attendance", action = "MemberOut" });
+});
+
+app.Run();
